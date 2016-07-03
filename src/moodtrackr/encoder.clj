f820c89af681fae6d6cbc7ee2548931f0602d65d
shotaroikeda <ikeda.shot@gmail.com>
2016-07-03 15:48:38 -0500

    added function to help encoding

--- src/moodtrackr/encoder.clj
+++ src/moodtrackr/encoder.clj
@@ -9,7 +9,33 @@
                     (range (int \0) (+ (int \9) 1))
                     [\+ \/])))
 
-;; Plumbing functions for
+;; Euclidean Algorithm
+(defn find-gcd
+  "Finds the Greatest Common Divisor of n1 and n2"
+  [n1 n2]
+  (loop [div n1
+         rep n2]
+    (if (= rep 0)
+      div
+      (recur rep (mod div rep)))))
+
+(defn find-lcm
+  "Finds the Least Common Multiple between n1 and n2"
+  [n1 n2]
+  (/ (* n1 n2) (find-gcd n1 n2)))
+
+(defn find-cycle
+  "Find the constant to determine how much padding needs to be applied."
+  [char-bits encoder-bits]
+  (/ (encoder/find-lcm 8 6) char-bits))
+
+(defn find-padding
+  "Finds the number of padding required, given how many bits per char and how many
+  bits per encoding char"
+  [ss char-bits encoder-bits]
+  (mod ss (find-cycle char-bits encoder-bits)))
+
+;; Plumbing functions for converting a string to base64 encoding
 (defn to-num-seq
   "Converts a String into a char-numeric sequence"
   [s]
@@ -48,8 +74,10 @@
 
 ;; Modify me for the last function call
 (defn str-to-bin
-  "Converts a string into a large binary string"
+  "Converts a string into a large binary string -- assumes 8 bits per char and 6 bits per encoding"
   [ss]
-  (-> ss
-      to-num-seq
-      to-bin-str))
+  (let [st-len (count ss)]
+    (-> (ss)
+        to-num-seq
+        to-bin-str
+        )))
