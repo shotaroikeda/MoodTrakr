f7e80a2e1898db2fa896169cdfd63ccb6ec319d5
shotaroikeda <ikeda.shot@gmail.com>
2016-07-07 10:41:11 -0500

    added coloring library for better output

---
+++ ansiterm.py
@@ -0,0 +1,30 @@
+def red(prt):
+    return "\033[91m{}\033[00m".format(prt)
+
+
+def green(prt):
+    return "\033[92m{}\033[00m".format(prt)
+
+
+def yellow(prt):
+    return "\033[93m{}\033[00m".format(prt)
+
+
+def light_purple(prt):
+    return "\033[94m{}\033[00m".format(prt)
+
+
+def purple(prt):
+    return "\033[95m{}\033[00m".format(prt)
+
+
+def cyan(prt):
+    return "\033[96m{}\033[00m".format(prt)
+
+
+def light_gray(prt):
+    return "\033[97m{}\033[00m".format(prt)
+
+
+def black(prt):
+    return "\033[98m{}\033[00m".format(prt)
